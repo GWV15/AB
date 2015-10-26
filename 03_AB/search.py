c2b2ff7c34e7e1cdbf88fde8c7ff3e083484c411
Maximilian Bauregger <4bauregg@informatik.uni-hamburg.de>
2015-10-26 16:00:00 +0100

    Two new functions: printField & serachStart

--- 03_AB/search.py
+++ 03_AB/search.py
@@ -2,4 +2,31 @@
 
 import sys
 
-lines = [line.rstrip('\n') for line in open(sys.argv[1])]
+#    0    5   10   15  19
+#    |    |    |    |   |     
+# 00-xxxxxxxxxxxxxxxxxxxx
+#    x                  x
+#    x       xxx        x
+#    x       x xxxxx    x
+#    x   s     x        x
+# 05-x       x x  xxxxxxx
+#    x  xx xxxxx        x
+#    x      x      g    x
+#    x      x           x
+# 09-xxxxxxxxxxxxxxxxxxxx
+
+field = [line.rstrip('\n') for line in open(sys.argv[1])]
+
+def printField(field):
+	for i in range(len(field)):
+		print(field[i])
+	pass
+
+
+def searchStart(field):
+	for i in range(len(field)):
+		for j in range(len(field[i])):
+			if field[i][j] == 's': return(i,j)
+
+print("Starting point at", searchStart(field))
+ 
