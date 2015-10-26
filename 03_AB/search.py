53995dc467ec954d237be17349407a0414d31ecb
C-gotoh <shigotoh1@gmail.com>
2015-10-26 16:14:37 +0100

    implement bfs
    to be refined

--- 03_AB/search.py
+++ 03_AB/search.py
@@ -1,43 +1,27 @@
 #!/usr/bin/python
 
-# Imports
 import sys
 
-# Functions
-def printField(field):
-	for i in range(len(field)):
-		print(field[i])
-	pass
+lines = [line.rstrip('\n') for line in open(sys.argv[1])]
 
-def searchFor(c,field):
-	for i in range(len(field)):
-		for j in range(len(field[i])):
-			if field[i][j] == c: return (i,j)
+goal = 'g'
 
-def searchStart(field):
-	return searchFor('s', field)
-
-
-#    0    5   10   15  19
-#    |    |    |    |   |     
-# 00-xxxxxxxxxxxxxxxxxxxx
-#    x                  x
-#    x       xxx        x
-#    x       x xxxxx    x
-#    x   s     x        x
-# 05-x       x x  xxxxxxx
-#    x  xx xxxxx        x
-#    x      x      g    x
-#    x      x           x
-# 09-xxxxxxxxxxxxxxxxxxxx
-
-# loading the field
-field = [line.rstrip('\n') for line in open(sys.argv[1])]
-print("Environment:")
-printField(field)
-
-c = 's'
-print("Character", c, "found at", searchFor(c,field))
-c = 'g'
-print("Character", c, "found at", searchFor(c,field))
- 
+def bfs(space):
+  start = searchstart(space)
+  frontier = [[start]]
+  while len(frontier)>0:
+    for path in frontier:
+      frontier.remove(path)
+      node = path[-1]
+      if space[node[0]][node[1]] = goal:
+        return path
+      if space[node[0]+1][node[1]+0] != x:
+        frontier.append(path.append(node[0]+1,node[1]+0))
+      if space[node[0]+0][node[1]+1] != x:
+        frontier.append(path.append(node[0]+0,node[1]+1))
+      if space[node[0]-1][node[1]+0] != x:
+        frontier.append(path.append(node[0]-1,node[1]+0))
+      if space[node[0]+0][node[1]-1] != x:
+        frontier.append(path.append(node[0]+0,node[1]-1))
+  # index out of bounds muss noch ausgeschlossen werden
+  return 0
