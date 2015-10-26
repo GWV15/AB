cfc5670522408e1785889a34e85f33bcfa26cb70
Maximilian Bauregger <4bauregg@informatik.uni-hamburg.de>
2015-10-26 16:16:53 +0100

    Merge

--- 03_AB/search.py
+++ 03_AB/search.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python
 
+# Imports
 import sys
 
-lines = [line.rstrip('\n') for line in open(sys.argv[1])]
+# Functions
+def printField(field):
+	for i in range(len(field)):
+		print(field[i])
+	pass
 
-goal = 'g'
+def searchFor(c,field):
+	for i in range(len(field)):
+		for j in range(len(field[i])):
+			if field[i][j] == c: return (i,j)
+
+def searchStart(field):
+	return searchFor('s', field)
 
 def bfs(space):
-  start = searchstart(space)
+  start = searchStart(space)
   frontier = [[start]]
   while len(frontier)>0:
     for path in frontier:
@@ -25,3 +36,28 @@ def bfs(space):
         frontier.append(path.append(node[0]+0,node[1]-1))
   # index out of bounds muss noch ausgeschlossen werden
   return 0
+
+
+#    0    5   10   15  19
+#    |    |    |    |   |     
+# 00-xxxxxxxxxxxxxxxxxxxx
+#    x                  x
+#    x       xxx        x
+#    x       x xxxxx    x
+#    x   s     x        x
+# 05-x       x x  xxxxxxx
+#    x  xx xxxxx        x
+#    x      x      g    x
+#    x      x           x
+# 09-xxxxxxxxxxxxxxxxxxxx
+
+# loading the field
+field = [line.rstrip('\n') for line in open(sys.argv[1])]
+print("Environment:\n")
+printField(field)
+print("\n")
+
+c = 's'
+print("Character", c, "found at", searchFor(c,field))
+c = 'g'
+print("Character", c, "found at", searchFor(c,field))
