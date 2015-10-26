6c0f4ec9870e670c9d81121b2505c1bc73d4a52e
Maximilian Bauregger <4bauregg@informatik.uni-hamburg.de>
2015-10-26 16:12:34 +0100

    Show the Environment

--- 03_AB/search.py
+++ 03_AB/search.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 
+# Imports
 import sys
+
+# Functions
+def printField(field):
+	for i in range(len(field)):
+		print(field[i])
+	pass
+
+def searchFor(c,field):
+	for i in range(len(field)):
+		for j in range(len(field[i])):
+			if field[i][j] == c: return (i,j)
+
+def searchStart(field):
+	return searchFor('s', field)
+
 
 #    0    5   10   15  19
 #    |    |    |    |   |     
@@ -15,18 +31,13 @@ import sys
 #    x      x           x
 # 09-xxxxxxxxxxxxxxxxxxxx
 
+# loading the field
 field = [line.rstrip('\n') for line in open(sys.argv[1])]
+print("Environment:")
+printField(field)
 
-def printField(field):
-	for i in range(len(field)):
-		print(field[i])
-	pass
-
-
-def searchStart(field):
-	for i in range(len(field)):
-		for j in range(len(field[i])):
-			if field[i][j] == 's': return(i,j)
-
-print("Starting point at", searchStart(field))
+c = 's'
+print("Character", c, "found at", searchFor(c,field))
+c = 'g'
+print("Character", c, "found at", searchFor(c,field))
  
