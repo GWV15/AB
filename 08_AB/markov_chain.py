206a84529edf0247925aa90c3fcd29da561cb6ca
C-gotoh <shigotoh1@gmail.com>
2015-11-30 15:49:54 +0100

    buildDictSlow

--- 08_AB/markov_chain.py
+++ 08_AB/markov_chain.py
@@ -30,13 +30,19 @@ def askForStart(cli_arg, firstQuestion, followingQuestion, wordlist):
     if cli_arg > 1 and len(sys.argv) > cli_arg:
         answer = sys.argv[cli_arg]
     else:
-        answer = input(firstQuestion + " (word in wordlist)")
+        answer = input(firstQuestion + " (word in wordlist) ")
 
     # Ask until valid answer is given
     while (answer not in wordlist):
-        answer = input(followingQuestion + " (word in wordlist)")
+        answer = input(followingQuestion + " (word in wordlist) ")
 
     return answer
+
+
+def buildDictSlow(wordlist):
+    wordset = set(wordlist)
+    dictionary = {word:[wordlist[min(len(wordlist)-1,wordlist.index(word)+1)]] for word in wordset}
+    return dictionary
 
 
 # ## Main method ##############################
@@ -44,18 +50,17 @@ def askForStart(cli_arg, firstQuestion, followingQuestion, wordlist):
 
 def main():
     # Load the field
-    # Load the field
     if len(sys.argv) >= 2:
-        wordlist = [line.rstrip('\n') for line in open(sys.argv[1])]
+        data = [line.rstrip('\n') for line in open(sys.argv[1])]
     else:
         print("There has to be at least one command line argument. \
             It should be a text file containig words.")
         return
     print(sys.argv[1])
 
-    # where_to_begin = askForStart(2, "Where do you want to start",
-    #    "This word is not in the wordlist", wordlist)
-
+    where_to_begin = askForStart(2, "Where do you want to start?",
+        "This word is not in the wordlist. Choose another.", data)
+    print("You are starting at ", where_to_begin)
 
 # Run the main method
 if __name__ == "__main__":
