1c8eb711155216243efe3b28baeede6bcd2eec8e
Maximilian Bauregger <max.bauregger@posteo.de>
2015-12-01 19:02:20 +0100

    [08_AB] Modified - Cleanup, restructuring, cli-arg handling updated

--- 08_AB/markov_chain.py
+++ 08_AB/markov_chain.py
@@ -7,49 +7,54 @@
 
 # Imports
 import sys
+import getopt
 import random
 
-# Constants
 
-# Functions
+def askForStart(dic, answer=""):
+    first_question = "Where do you want to start? (word in txt) "
+    following_question = " is not in the wordlist. Choose another. "
 
+    if answer == "":
+        answer = input(first_question)
 
-def askForAnswer(cli_arg, first_question, following_question, answers=[]):
-    if cli_arg > 1 and len(sys.argv) > cli_arg:
-        answer = sys.argv[cli_arg]
-    else:
-        answer = input(first_question + " (" + "|".join(answers) + ") ")
-
-    # Ask until valid answer is given
-    while(answer not in answers):
-        answer = input(following_question + " (" + "|".join(answers) + ") ")
+    while (answer not in dic.keys()) or answer is "":
+        answer = input('"' + answer + '"' + following_question)
 
     return answer
 
 
-def askForStart(cli_arg, first_question, following_question, dic):
-    if cli_arg > 1 and len(sys.argv) > cli_arg:
-        answer = sys.argv[cli_arg]
-    else:
-        answer = input(first_question + " (word in wordlist) ")
+def askForNumber(min, max, init_val=0):
+    try:
+        word_number = int(init_val)
+    except ValueError:
+        word_number = 0
 
-    # Ask until valid answer is given
-    while (answer not in dic.keys()):
-        answer = input(following_question + " (word in wordlist) ")
-    return answer
+    question = "How many words? "
+    while word_number > max or word_number < min:
+        try:
+            word_number = int(input(question))
+        except ValueError:
+            continue
+
+    return word_number
 
 
-def buildDictSlow():
-    wordlist = [line.rstrip('\n') for line in open(sys.argv[1])]
+def buildDictSlow(text_file):
+    wordlist = [line.rstrip('\n') for line in open(text_file)]
     wordset = set(wordlist)
-    dictionary = {word:[wordlist[min(len(wordlist)-1,wordlist.index(word)+1)]] for word in wordset}
+
+    dictionary = {word: [wordlist[min(len(wordlist) - 1,
+                  wordlist.index(word)+1)]] for word in wordset}
+
     return dictionary
 
 
 def builtDict(text_file):
     dictionary = {}
+
     previous = ""
-    for word in open(text_file):
+    for word in text_file:
         word = word.rstrip('\n')
 
         # Filter some words out
@@ -60,60 +65,93 @@ def builtDict(text_file):
             dictionary[previous] = [word]
         else:
             dictionary[previous].append(word)
+
         previous = word
+
     return dictionary
 
 
 def countWords(dictionary, word):
     wordcount = {}
+
     for nextword in dictionary.get(word):
         if nextword not in wordcount:
             wordcount[nextword] = 1
         else:
             wordcount[nextword] += 1
+
     return wordcount
 
 
 def generateSentence(wstart, length, dic):
     sentence = [wstart]
+
     while len(sentence) < length:
         sentence.append(random.choice(dic.get(sentence[-1])))
+
     return sentence
 
 
-# ## Main method ##############################
+def buildSentenceString(sentence_list, no_space_list):
+    sentence_string = sentence_list[0]
+
+    for word in sentence_list[1:]:
+        if word not in no_space_list:
+            word = " " + word
+        sentence_string = sentence_string + word
+
+    if sentence_string[-1] != '.':
+        sentence_string = sentence_string + '.'
+
+    return sentence_string
+
+
+def evalCmdArg(cmd_list):
+    input_file = ""
+    word_number = ""
+    start_word = ""
+
+    # text file needed
+    if len(sys.argv) < 2:
+        print("The first argument has to be the text file.")
+        sys.exit(2)
+    else: 
+        try:
+            input_file = open(sys.argv[1])
+        except IOError:
+            print("The text file could not be read")
+            sys.exit(2)
+
+        # one of two optional arguments given
+        if len(sys.argv) == 3:
+            try:
+                word_number = int(sys.argv[2])
+            except ValueError:
+                # the second argument is not the word number
+                start_word = str(sys.argv[2])
+
+        # two of two optional arguments given
+        elif len(sys.argv) >= 4:
+            try:
+                word_number = int(sys.argv[2])
+                start_word = str(sys.argv[3])
+            except ValueError:
+                # the second argument is not the word number
+                start_word = str(sys.argv[2])
+                word_number = int(sys.argv[3])
+
+    return input_file, start_word, word_number
 
 
 def main():
-    # Load the field
-    if len(sys.argv) != 4:
-        if len(sys.argv) == 3:
-            dic = builtDict(sys.argv[1])
-            where_to_begin = random.choice(list(dic.keys()))
-            number = int(sys.argv[2])
+    input_file, start_word, word_number = evalCmdArg(sys.argv)
 
-        else:
-            print("Usage: text-file Start-word(optional) Length \n")
-            return None
-    else:
-        dic = builtDict(sys.argv[1])
-        where_to_begin = str(sys.argv[2])
-        number = int(sys.argv[3])
+    dic = builtDict(input_file)
+    start_word = askForStart(dic, start_word)
+    word_number = askForNumber(3, 500, word_number)
 
-        # print(sorted(countWords(dic, 'als').items(), key=lambda x: x[1]))
-
-    #where_to_begin = askForStart(2, "Where do you want to start?",
-    #                             "This word is not in the wordlist.\
-    #                             Choose another.", dic)
-    #print("You are starting at ", where_to_begin)
-
-    #number = int(input("How many words should the sentence contain?"))
-    #while number > 500 or number < 3:
-    #    number = int(input("Again. How many words?"))
-
-    sentence_string = ""
-    for word in generateSentence(where_to_begin, number, dic):
-        sentence_string = sentence_string + " " + word
+    sentence_list = generateSentence(start_word, word_number, dic)
+    sentence_string = buildSentenceString(sentence_list, ['.', ',', ';', ':'])
     print(sentence_string)
 
 # Run the main method
