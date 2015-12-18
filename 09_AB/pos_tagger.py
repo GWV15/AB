4d65a39835051861c137a72936a09fcead33984a
Maximilian Bauregger <max.bauregger@posteo.de>
2015-12-18 04:28:25 +0100

    [09_AB] Modif - 1.2.2/1.2.3

--- 09_AB/pos_tagger.py
+++ 09_AB/pos_tagger.py
@@ -7,133 +7,59 @@
 
 # Imports
 import sys
-import random
 
-
-def getWordList(dic):
-    words = []
-    for key in dic.keys():
-        for word in key.split("#")[1:]:
-            words.append(word)
-    return words
 
 # Build a dictionary
 #
 # every word (combination of length ngramlvl) in text_file is a key.
 # For each key, value is a list with possible following words
-def builtDict(text_file, ngramlvl):
-    dictionary = {}
+def buildDicts(data_file):
+    dict_tags = {}
+    dict_words = {}
 
-    lastlist = []
-    for word in text_file:
-        word = word.rstrip('\n')
-
-        # Filter some words out
-        if word in []:
-            next
-
-        key = genKey(lastlist)
-        if key not in dictionary:
-            dictionary[key] = [word]
-        else:
-            dictionary[key].append(word)
-
-        if len(lastlist) >= ngramlvl - 1:
-            lastlist.pop(0)
-
-        lastlist.append(word)
-
-    return dictionary
-
-# Build a dictionary
-#
-# every word (combination of length ngramlvl) in text_file is a key.
-# For each key, value is a list with possible following words
-def buildTagDict(data_file):
-    ngramlvl = 2
-    dictionary = {}
-
-    lastlist = []
+    prev_tag = '$.'
     for line in data_file:
-        tag = line.strip().split('\t')[-1]
-        if (len(tag) != 0): # if empty line in data
-            #print(tag)
-            # Filter some words out
-            if tag in []:
-                next
 
-            key = genKey(lastlist)
-            if key not in dictionary:
-                dictionary[key] = [tag]
+        word_tag = line.strip().split('\t')
+        if len(word_tag) < 2:
+            continue
+
+        word = word_tag[0]
+        tag = word_tag[1]
+
+        if len(tag) != 0:
+            if word not in dict_words:
+                dict_words[word] = [tag]
             else:
-                dictionary[key].append(tag)
+                dict_words[word].append(tag)
 
-            if len(lastlist) >= ngramlvl - 1:
-                lastlist.pop(0)
+            if prev_tag not in dict_tags:
+                dict_tags[prev_tag] = [tag]
+            else:
+                dict_tags[prev_tag].append(tag)
+            prev_tag = tag
 
-            lastlist.append(tag)
-    return dictionary
+    return dict_tags, dict_words
 
-def genKey(wordlist):
-    key = ""
-    for item in wordlist:
-        key = key + "#" + item
-    return key
-
-def getStartTag(dic,text):
-    startTag = "NN"
-    return startTag
 
 # This is were the magic happens
 # generate a sentence (list).
-def tagText(dic, textlist):
-    ngramlvl = 2
-    length = len(textlist)
-    wstart = getStartTag(dic,textlist)
-    taglist = []
-    if 2 < ngramlvl:
-        possible_keys = []
-        for x in dic.keys():
-            if x is '':
-                continue
-            words = x.split("#")[1:]
-            if words[0] == wstart:
-                possible_keys.append(words)
+def tagText(text, dict_words, dict_tags):
+    tags = ['$.']
 
-        taglist = taglist.append(random.choice(possible_keys))
-    else:
-        taglist = [wstart]
-    while len(taglist) < length or taglist is []:
-        key = genKey(taglist[-(ngramlvl-1):])
-        if key not in dic:
-            taglist = taglist[:-1]
-            continue
-        taglist.append(random.choice(dic.get(key)))
+    for word in text:
+        if word not in dict_words:
+            print(word + " not in dictionary. Previous tag: " + tags[-1])
+            
+            newtag = dict_tags[tags[-1]][0]
+            print("Inserting " + newtag + " instead")
+            
+            tags.append(newtag)
 
-    return taglist
+        else:
+            tags.append(dict_words[word][0])
 
-def buildTextList(text_file):
-    list = []
-    for line in text_file:
-        word = line.split()
-        list.append(word)
-    return list[0]
-
-# Turn the sentence list into a string
-# (and make some cosmetic changes)
-def buildSentenceString(sentence_list, no_space_list):
-    sentence_string = sentence_list[0]
-    for word in sentence_list[1:]:
-        if word not in no_space_list:
-            word = " " + word
-        sentence_string = sentence_string + word
-
-    if sentence_string[-1] in no_space_list:
-        sentence_string = sentence_string[:-1]
-
-    sentence_string = sentence_string + '.'
-
-    return sentence_string
+    return tags[1:]
 
 
 # Evaluate the arguments given to the program
@@ -142,40 +68,61 @@ def evalCmdArg(cmd_list):
     text_file = ""
 
     # text file needed
-    if len(sys.argv) < 3:
-        print("The first argument has to be the data_file. The second one the text to tag.")
-        #sys.exit(2)
-    else:
-        try:
-            data_file = open(sys.argv[1])
-        except IOError:
-            print("The text file could not be read")
-            sys.exit(2)
+    if len(sys.argv) < 2:
+        print("Too few arguments. The first argument has to be the data file.\
+               The second mey be the text to tag.")
+        sys.exit(2)
 
-        try:
-            text_file = open(sys.argv[2])
-        except ValueError:
-            print("The text file could not be read")
-            sys.exit(2)
+    try:
+        data_file = open(sys.argv[1])
+    except IOError:
+        print("The data file could not be read")
+        sys.exit(2)
+
+    if len(sys.argv) == 3:
+        text_file = sys.argv[2]
+
+    if len(sys.argv) > 3:
+        print("Too many arguments. The first argument has to be the data file.\
+               The second may be the text to tag.")
 
     return data_file, text_file
+
+
+def splitText(text):
+    words = []
+    word = ""
+
+    for char in text.strip():
+        if char == ' ':
+            words.append(word)
+            word = ""
+            next
+        elif char in ['.', ',', ';', ':', '!', '?']:
+            words.append(word)
+            word = char
+        else:
+            word = word + char
+
+    return words
 
 
 # Run the program
 #
 # Usage:
-#  $> pos_tagger.py path/to/trainingdata path/to/untagged/text
+#  $> pos_tagger.py path/to/trainingdata "TEXT_TO_TAG"
 #
 def main():
-    data_file, text_file = evalCmdArg(sys.argv)
+    data_file, text = evalCmdArg(sys.argv)
 
+    dict_tags, dict_words = buildDicts(data_file)
 
-    text = buildTextList(text_file)
-    dic = buildTagDict(data_file)
-    taggedText = "not yet"
-    taggedText = tagText(dic,text)
+    words = splitText(text)
+
+    tags = tagText(words, dict_words, dict_tags)
+
     print(text)
-    print(taggedText)
+    print(tags)
 
 
 # Run the main method
